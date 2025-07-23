2d36cb4930533719d52cba60827a2ad370edf941
Perhan <perhan@local>
2025-07-23 22:00:40 +0300

    added config with predefined data source url and auth bearer token

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'data_source_url' => 'https://data.gov.lv/dati/dataset/40d80be5-0c09-47c4-80f3-fad4bec19f33/resource/c32c7afd-0d05-44fd-8b24-1de85b4bf11d/download/meteo_stacijas.csv',
+    'valid_bearer_token' => '1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p',
+];
